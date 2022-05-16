63211c1d157e4e47822d013a8e8aa131eb34452a
Arnaudgouel <gouel.arnaud@gmail.com>
2022-05-16 23:55:22 +0200

    login background

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -12,6 +12,8 @@ const Login = () => {
   const {setToken, token} = useToken()
   const location = useLocation()
 
+  let background = require(`../assets/img/background casdal login.png`)
+
   let from = location.state?.from?.pathname || "/"
 
   const handleSubmit = (e) => {
@@ -32,27 +34,29 @@ const Login = () => {
       .catch(error => setError(error.response.data))
   }
   return (
-    <div className="position-absolute top-50 start-50 translate-middle bg-light shadow-lg text-center p-3">
-      {error && 
-        <div className="alert alert-danger">
-          Les identifiants ne sont pas corrects
-        </div>
-      }
-      <img className="img-fluid w-25" src={logo} alt="logo" />
-      <div className="mt-3 fs-3">Connexion</div>
-      <form className="needs-validation" onSubmit={(e) => handleSubmit(e)}>
-        <label className="form-label" htmlFor="email">Email</label>
-        <input type="email" className="form-control" id="email" value={email} onInput={e => setEmail(e.target.value)} required/>
-        <div className="invalid-feedback">
-          Veullez renseigner un email
-        </div>
-        <label className="form-label" htmlFor="password">Mot de passe</label>
-        <input type="password" className="form-control" id="password" value={password} onInput={e => setPassword(e.target.value)} required/>
-        <div className="invalid-feedback">
-          Veullez renseigner un mot de passe
-        </div>
-        <button className="btn btn-primary mt-2">Se connecter</button>
-      </form>
+    <div className="w-100 h-100" style={{backgroundImage: `url("${background}")`, backgroundSize: "cover"}}>
+      <div className="position-absolute top-50 start-50 translate-middle bg-light shadow-lg text-center p-3">
+        {error &&
+          <div className="alert alert-danger">
+            Les identifiants ne sont pas corrects
+          </div>
+        }
+        <img className="img-fluid w-25" src={logo} alt="logo" />
+        <div className="mt-3 fs-3">Connexion</div>
+        <form className="needs-validation" onSubmit={(e) => handleSubmit(e)}>
+          <label className="form-label" htmlFor="email">Email</label>
+          <input type="email" className="form-control" id="email" value={email} onInput={e => setEmail(e.target.value)} required/>
+          <div className="invalid-feedback">
+            Veullez renseigner un email
+          </div>
+          <label className="form-label" htmlFor="password">Mot de passe</label>
+          <input type="password" className="form-control" id="password" value={password} onInput={e => setPassword(e.target.value)} required/>
+          <div className="invalid-feedback">
+            Veullez renseigner un mot de passe
+          </div>
+          <button className="btn btn-primary mt-2">Se connecter</button>
+        </form>
+      </div>
     </div>
   )
 }
